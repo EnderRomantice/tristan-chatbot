7a066bd00a8974a7ceb336f018d882fd800e18de
EnderRomantice <enderromantic@gmail.com>
2026-03-08 19:39:05 +0800

    style: remove app.tsx text-black

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { ChatInterface } from './components/Chat/ChatInterface';
 
 function App() {
   return (
-    <div className="w-full h-screen font-sans antialiased text-black">
+    <div className="w-full h-screen font-sans antialiased">
       <ChatInterface />
     </div>
   );
